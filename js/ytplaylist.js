0434875019b14ca9d35703ae1493f1cdf72de0ea
Tom Britton <tom.britton@wholefoods.com>
2013-04-26 16:12:09 -0500

    Basic functionality for a YT playlist

--- js/ytplaylist.js
+++ js/ytplaylist.js
@@ -1,31 +1,123 @@
 (function( $, undefined ) {
   $.fn.ytPlaylist = function(options) {
-  
+    
   	var settings = $.extend( {
   		'playlist' : 'PLDFmQ1Ea1sUXBNTLloUE6T0iSs5Xt97Ig',
   	}, options);
 
   	var request = 'https://gdata.youtube.com/feeds/api/playlists/' + settings.playlist + '?alt=json'
     
-    var getJSON = function(request) {
+    var getJSON = function(request, selector) {
     	$.ajax({
   			url: request,
   			dataType: 'json',
   			success: function(response){
-    			successCallback(response);
+    			successCallback(response, selector);
   			},
   			error: function(jqXHR, textStatus, errorThrown){
   				console.log(textStatus);
   			},
     	});
-    }	
+    };
 
-    var successCallback = function(json){
-    	console.log(json);
+    /**
+    *  Success callback for AJAX request. Calls functions that process returned JSON.
+    *  
+    *  @param object json
+    *    JSON object returned by YouTube API.
+    *  @param string selector
+    *    Element in which to create playlist.  
+    */
+    var successCallback = function(json, selector) {
+    	createTitle(json, selector);
+      parseList(json, selector);
+      addListeners(selector);
+    };
+
+    /**
+    *  Create title and append it to playlist container element
+    *   
+    *  @param object json
+    *    JSON object returned by YouTube API.
+    *  @param string selector
+    *    Element in which to create playlist.  
+    */
+    var createTitle = function(json, selector) {
+      var title = json.feed.title.$t;
+      $(selector).append('<h2>' + title + '</h2>');
+    };
+	  
+    /**
+    *  Create iframe embed and list of videos from JSON
+    *   
+    *  @param object json
+    *    JSON object returned by YouTube API.
+    *  @param string selector
+    *    Element in which to create playlist.  
+    */
+    var parseList = function(json, selector) {
+      var list = json.feed.entry;
+      $(list).each(function( index ) {
+        var vidinfo = [];
+        vidinfo.url = $(this)[0].link[0].href;
+        vidinfo.height = $(this)[0].media$group.media$thumbnail[0].height;
+        vidinfo.width = $(this)[0].media$group.media$thumbnail[0].width;
+        vidinfo.title = $(this)[0].title.$t;
+        if(index === 0) {
+          embedIframe(vidinfo, selector);
+          $(selector).append('<ul></ul>');
+        }
+        var listItem = '<li><a href="' + vidinfo.url + '" rel=\'{"h":' + vidinfo.height+ ', "w":' + vidinfo.width + '}\'>' + vidinfo.title + '</a></li>';
+        $(selector).children('ul').append(listItem);
+      });
+    };
+
+    /**
+    *  Create iframe embed and list of videos from JSON
+    *   
+    *  @param object vidinfo
+    *    Object of video info used in the embed
+    *  @param string selector
+    *    Element in which to embed iframe.  
+    */
+    var embedIframe = function(vidinfo, selector) {
+      //Get rid of any existing iframes
+      $(selector).children('iframe').remove();
+      var urlsplit = vidinfo.url.split(/v=|&/);
+      vid = urlsplit[1];
+      var width = $(selector).width();
+      if(width > vidinfo.width) {
+        var proportion = vidinfo.width / width;
+        var difference = (1 - proportion);
+        var height = Math.round(vidinfo.height * difference + vidinfo.height);
+      } else {
+        var proportion = width / vidinfo.width;
+        var difference = (1 - proportion);
+        var height = Math.round(vidinfo.height * difference);
+      }
+      var iframe = '<iframe width="' + width + '" height="' + height + '" src="http://www.youtube.com/embed/' + vid + '" frameborder="0" allowfullscreen></iframe>';
+      $(selector).prepend(iframe);
+    };
+    
+    var addListeners = function(selector) {
+      $(selector).find('a').bind('click', function(){
+        event.preventDefault();
+        onClickCallback($(this), selector);
+        return false;
+      });
     }
 
-	getJSON(request);
-
-
+    var onClickCallback = function(element, selector) {
+      var vidinfo = []
+      $(selector).find('a').removeClass('active');
+      $(element).addClass('active');
+      vidinfo.url = $(element).attr('href');
+      var dimensions = $.parseJSON($(element).attr('rel'));
+      vidinfo.height = dimensions.h;
+      vidinfo.width = dimensions.w;
+      embedIframe(vidinfo, selector);
+    }
+    //Kick it off
+    getJSON(request, $(this));
   };
 })( jQuery );
